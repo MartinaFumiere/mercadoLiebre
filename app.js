44b399f4e6c6407e38e6f62e21d6605db96e7999
MartinaFumiere <martifumiere97@gmail.com>
2021-09-16 17:27:55 -0300

    primercomm

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require ("express");
+
+const app = express();
+
+const path = require ('path');
+const publicPath = path.resolve(__dirname, './public');
+app.use(express.static(publicPath));
+
+const port = 3000;
+app.listen (port, () => console.log ("Hola servidor"));
+
+app.post('/', (req,res) => {
+res.sendFile(path.resolve(__dirname, 'views/home.html'));
+})
+app.get('/register', (req,res) => {
+    res.sendFile(path.resolve(__dirname, 'views/register.html'));
+    })
+    app.get('/login', (req,res) => {
+        res.sendFile(path.resolve(__dirname, 'views/login.html'));
+        })
+app.get('/', (req,res) => {
+            res.sendFile(path.resolve(__dirname, 'views/home.html'));
+            })
