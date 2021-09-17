07ef3aadf997168bcff1f647f1e3562e717a32d7
MartinaFumiere <martifumiere97@gmail.com>
2021-09-16 22:40:03 -0300

    pruebo

--- app.js
+++ app.js
@@ -5,10 +5,9 @@ const app = express();
 const path = require ('path');
 const publicPath = path.resolve(__dirname, './public');
 app.use(express.static(publicPath));
+const PORT = process.env.PORT || 3000;
 
-const port = 3000;
-app.listen (port, () => console.log ("Hola servidor"));
-
+app.listen(process.env.PORT || 3000, function(){console.log('hola servidor')})
 app.post('/', (req,res) => {
 res.sendFile(path.resolve(__dirname, 'views/home.html'));
 })
