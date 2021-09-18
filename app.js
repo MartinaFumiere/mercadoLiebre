2f8e4140a05a25c3ff9e4552fd48c7e5af34daf0
MartinaFumiere <martifumiere97@gmail.com>
2021-09-18 11:32:33 -0300

    modificando

--- app.js
+++ app.js
@@ -6,8 +6,9 @@ const path = require ('path');
 const publicPath = path.resolve(__dirname, './public');
 app.use(express.static(publicPath));
 const PORT = process.env.PORT || 3000;
+app.listen(process.env.PORT || 3000, function(){console.log('Funciona en el servidor')
+})
 
-app.listen(process.env.PORT || 3000, function(){console.log('hola servidor')})
 app.post('/', (req,res) => {
 res.sendFile(path.resolve(__dirname, 'views/home.html'));
 })
